02d3409e6b9d35d3a3ff770a27c4d7c9a3e963b4
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2024-11-12 12:00:14 -0500

    Create setup.py

---
+++ Codes/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='APanda_Lib',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='A PANDA',
+    author_email='ashmita.panda@niser.ac.in',
+    description=''
+)
